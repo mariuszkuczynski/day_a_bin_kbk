6b2ed97ae144e298a7173c887bba2019a32d5107
Mariusz Kuczyński <kuczynski.mariusz@gmail.com>
2022-08-18 10:46:15 +0200

    Install Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'day a bin kbk',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: const Scaffold(),
