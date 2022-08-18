a35fd082ea81c8291b277da5d200d2e3c66e7c54
Mariusz Kuczyński <kuczynski.mariusz@gmail.com>
2022-08-18 15:51:51 +0200

    Add Login functionality

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -1,19 +1,78 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({
+  LoginPage({
     Key? key,
   }) : super(key: key);
 
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Jesteś nie zalogowany',
+          child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Zaloguj się',
+              style: GoogleFonts.inriaSerif(fontSize: 20),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromRGBO(173, 198, 206, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Email',
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: const Color.fromRGBO(173, 198, 206, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Hasło',
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  primary: const Color.fromRGBO(173, 198, 206, 1),
+                  textStyle: GoogleFonts.inriaSerif(color: Colors.black)),
+              onPressed: () async {
+                try {
+                  await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: emailController.text,
+                      password: passwordController.text);
+                } catch (error) {
+                  print(error);
+                }
+              },
+              child: const Text('Zaloguj się'),
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
